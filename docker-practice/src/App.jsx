a43a3b6c8c1d9af33bbb3a3a228f4404ad11d14a
Showrov <yshowrov7@gmail.com>
2025-03-21 04:05:36 +0600

    docker volume, bind mount, communication from to container

--- docker-practice/src/App.jsx
+++ docker-practice/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts/2')
+      .then(response => setPost(response.data))
+      .catch(error => console.error('Error:', error));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>This is Docker App!!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {post ? <h2>{post.title}</h2> : <p>Loading...</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
